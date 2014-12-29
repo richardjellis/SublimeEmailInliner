d7e33cea217362a03b448e143d0834725754267f
Rich Ellis <rich@rje.me>
2014-12-29 14:57:06 +0000

    Add configuration options for Premailer API

--- EmailInliner.py
+++ EmailInliner.py
@@ -1,12 +1,21 @@
 import sublime, sublime_plugin, urllib, json
 
+
 class EmailInlineCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		region = sublime.Region(0, self.view.size())
 		content = self.view.substr(region)
 
 		api_url = 'http://premailer.dialect.ca/api/0.1/documents'
-		values = { 'html' : content }
+		values = { 	'html' : content,
+					'adapter' : view.settings().get('ei_premailer_adapter'),
+					'base_url' : view.settings().get('ei_premailer_base_url'),
+					'link_query_string' : view.settings().get('ei_premailer_link_query_string'),
+					'preserve_styles' : view.settings().get('ei_premailer_preserve_styles'),
+					'remove_ids' : view.settings().get('ei_premailer_remove_ids'),
+					'remove_clases' : view.settings().get('ei_premailer_remove_classes'),
+					'remove_comments' : view.settings().get('ei_premailer_remove_comments') }
+
 		data = urllib.parse.urlencode(values)
 		data = data.encode('utf-8')
 		req = urllib.request.urlopen(api_url, data)
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A simple Sublime Text plugin to inline HTML email styles using the Premailer API
 
 Install the awesome Sublime Text [Package Control](https://sublime.wbond.net/installation) and add this repo.
 
+##Configuration
+
+Settings for the Premailer API can be found in EmailInliner.sublime-settings. Override these in User/EmailInline.sublime-settings.
+
 ##Usage
 
 Run the Email Inliner command. Wait. Hey presto, your HTML email is inlined.
